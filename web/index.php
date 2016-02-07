fb4178bbf61f3940718dcd4e932e3c857dcc0ffe
Vítězslav Dvořák <info@vitexsoftware.cz>
2016-02-07 01:10:34 +0100

    Title page background image

--- web/index.php
+++ web/index.php
@@ -12,23 +12,29 @@ namespace SkeliCZ;
 
 require_once 'includes/SkeliInit.php';
 
+$oPage->addCss('body { background-image: url("images/skelilbackmic.png"); background-position: left top;  background-repeat: no-repeat; }');
 
 $oPage->addItem(new PageTop(_('MC Skeli')));
 
-$oPage->container->addItem(new \Ease\Html\H1Tag(_('News')));
+$indexrow = new \Ease\TWB\Row();
+$newscolumn = $indexrow->addColumn(6, new \Ease\Html\H1Tag(_('News')));
 
+$welcometext = $newscolumn->addItem(new \Ease\Html\Div(_('Welcome on my new website!'),['class'=>'smokeback']));
+$welcometext->addItem( new \Ease\Html\Div( _('For ten years I have been a rap music.')));
 
-$oPage->container->addItem(new \Ease\Html\Div(
-        new \Ease\Html\IframeTag('https://www.youtube.com/embed/auNofZzBIFA' 
+$videocolumn = $indexrow->addColumn(6, new \Ease\Html\Div(
+        new \Ease\Html\IframeTag('https://www.youtube.com/embed/auNofZzBIFA'
         , [
     'width' => "540",
     'height' => "380",
     'frameborder' => "0",
-    'allowfullscreen'=>true,
+    'allowfullscreen' => true,
     'showinfo' => 1,
         ]
-        ),['style'=>'text-align: center;','class'=>'videoWrapper'])
+        ), ['style' => 'text-align: center;', 'class' => 'videoWrapper'])
 );
+
+$oPage->container->addItem($indexrow);
 
 $oPage->addItem(new PageBottom());
 
